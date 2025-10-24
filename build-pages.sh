d9cc5d1003f0b0291755b4573e8d1e124392d283
Tai Kedzierski <dch.tai@gmail.com>
2025-10-24 17:16:05 +0100

    Generate to correct location

--- build-pages.sh
+++ build-pages.sh
@@ -3,10 +3,13 @@
 cd "$(dirname "$(realpath "$0")")"
 
 mkdir -p web
+( set -x
 ./run.sh -o web/articles-today.json
-./run.sh -l web/articles-today.json -t web/howtos.html howtos
-./run.sh -l web/articles-today.json -t web/security.html security
-./run.sh -l web/articles-today.json -t web/golang.html golang
-./run.sh -l web/articles-today.json -t web/programming.html "programming leftovers"
+./run.sh -l web/articles-today.json -t web/htdocs/tuxmachines.html
+./run.sh -l web/articles-today.json -t web/htdocs/howtos.html howtos
+./run.sh -l web/articles-today.json -t web/htdocs/security.html security
+./run.sh -l web/articles-today.json -t web/htdocs/golang.html golang
+./run.sh -l web/articles-today.json -t web/htdocs/programming.html "programming leftovers"
+)
 
 bash create-index.sh
