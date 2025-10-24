58079395fe49758a9a05affb5a14ca311382ab95
Tai Kedzierski <dch.tai@gmail.com>
2025-10-24 17:16:05 +0100

    Declare charset

--- htmlgen/__init__.py
+++ htmlgen/__init__.py
@@ -2,6 +2,7 @@ from htmlgen.htmlgen import html_summary_gen
 
 HEADER = """<html>
 <head><title>Tux Machines {filter_name}</title>
+<meta charset="utf-8">
 <link rel="stylesheet" href="tuxmachines.css">
 </head><body>
 <p style="float: left"></p>
