e91193a34adda51388c0e02dd7c39bdf026214ec
Tai Kedzierski <dch.tai@gmail.com>
2025-10-24 17:16:05 +0100

    Add venv management to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 set -euo pipefail
 
 HEREDIR="$(dirname "$(realpath "$0")")"
+VDIR="$HEREDIR/py3.venv"
 
-. "$HEREDIR/py3.venv/bin/activate"
+activate() {
+. "$VDIR/bin/activate"
+}
+
+if [[ ! -f "$VDIR/bin/activate" ]]; then
+    python3 -m venv "$VDIR"
+    (
+        activate
+        pip install -r requirements.txt
+    )
+fi
+
+activate
 
 python3 "$HEREDIR/tm-reader.py" "$@"
